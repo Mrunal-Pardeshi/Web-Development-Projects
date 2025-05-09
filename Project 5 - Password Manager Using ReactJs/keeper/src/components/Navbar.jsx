1427e20ab03feeec870cf254ab7912c340b154ee
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-05-09 15:44:56 +0530

    In Progress

--- Project 5 - Password Manager Using ReactJs/keeper/src/components/Navbar.jsx
+++ Project 5 - Password Manager Using ReactJs/keeper/src/components/Navbar.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-[#7877c64d] flex justify-between py-3 px-5  text-white items-center'>
+    <nav className='bg-[#7877c64d] flex justify-between py-3 px-5 h-16 text-white items-center'>
         <div className="logo font-bold text-2xl">
           <span className='text-green-500'>&lt;</span>
           <span>KEEPER</span>
           <span className='text-green-500'>/&gt;</span>
           </div>
-        <ul className=''>
+        {/* <ul className=''>
             <li className='gap-5 flex'>
                 <a className='hover:font-bold' href="/">Home</a>
                 <a className='hover:font-bold' href="/">Contact</a>
                 <a className='hover:font-bold' href="/">About Us</a>
             </li>
-        </ul>
+        </ul> */}
+        <button className='flex bg-green-500 justify-between items-center px-2 rounded-full cursor-pointer hover:bg-green-700'>
+          <img className='invert w-10 p-2' src="/icons/github.svg" alt="" />
+          <span className='font-bold'>GitHub</span>
+        </button>
     </nav>
   )
 }
