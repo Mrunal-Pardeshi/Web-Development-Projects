ef45e05c6a62547cfbbffa91b00fad9e338bb940
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-05-13 11:34:24 +0530

    In Progress

--- Project 5 - Password Manager Using ReactJs/keeper-mongoDB/backend/server.js
+++ Project 5 - Password Manager Using ReactJs/keeper-mongoDB/backend/server.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'keeper';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+client.connect();
+    
+
+
+app.get('/', async (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+    const findResult = await collection.find({}).toArray();
+    res.json(findResult)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 }) 
