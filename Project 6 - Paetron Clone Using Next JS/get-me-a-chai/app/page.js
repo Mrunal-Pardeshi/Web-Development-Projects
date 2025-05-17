d6b70ae002cd347fd4098f532d48b94f34f7ed41
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-05-17 17:37:36 +0530

    In Progress

--- Project 6 - Paetron Clone Using Next JS/get-me-a-chai/app/page.js
+++ Project 6 - Paetron Clone Using Next JS/get-me-a-chai/app/page.js
@@ -9,19 +9,27 @@ export default function Home() {
       A crowdfunding platform for creators. Fund your project and get funded. Start Now!
      </p>
      <div>
-      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
-      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
      </div>
     </div>
     <div className="bg-white h-1 opacity-10">
     </div>
 
     <div className="text-white">
-      <h1>Your Fans can buy you a chai</h1>
-      <div className="flex gap-5">
-        <div className="item">
-          <img src="/man.gif" width={88} alt="" />
-          <p>Fund Yourself</p>
+      <h1 className="text-2xl font-bold text-center my-4">Your Fans can buy you a chai</h1>
+      <div className="flex gap-5 justify-around">
+        <div className="item space-y-3">
+          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
+          <p className="font-bold">Fund Yourself</p>
+        </div>
+        <div className="item space-y-3">
+          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
+          <p className="font-bold">Fund Yourself</p>
+        </div>
+        <div className="item space-y-3">
+          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
+          <p className="font-bold">Fund Yourself</p>
         </div>
       </div>
     </div>
