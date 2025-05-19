c2278a6e3dee71b16e8e00b9b8efacbdcb27a227
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-05-19 11:34:31 +0530

    In Progress

--- Project 6 - Paetron Clone Using Next JS/get-me-a-chai/app/page.js
+++ Project 6 - Paetron Clone Using Next JS/get-me-a-chai/app/page.js
@@ -16,20 +16,21 @@ export default function Home() {
     <div className="bg-white h-1 opacity-10">
     </div>
 
-    <div className="text-white">
-      <h1 className="text-2xl font-bold text-center my-4">Your Fans can buy you a chai</h1>
+    <div className="text-white container mx-auto ">
+      <h1 className="text-2xl font-bold text-center my-12">Your Fans can buy you a chai</h1>
       <div className="flex gap-5 justify-around">
         <div className="item space-y-3">
           <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
           <p className="font-bold">Fund Yourself</p>
         </div>
         <div className="item space-y-3">
-          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
+          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/coin.gif" width={88} alt="" />
           <p className="font-bold">Fund Yourself</p>
         </div>
-        <div className="item space-y-3">
-          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/man.gif" width={88} alt="" />
-          <p className="font-bold">Fund Yourself</p>
+        <div className="item space-y-3 flex flex-col items-center justify-center">
+          <img className=" bg-slate-500 rounded-full p-3 text-black" src="/group.gif" width={88} alt="" />
+          <p className="font-bold">Your Fans want to help</p>
+          {/* <p>You fans are aavailable for you to help</p> */}
         </div>
       </div>
     </div>
