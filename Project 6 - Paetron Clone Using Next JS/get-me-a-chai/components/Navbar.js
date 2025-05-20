530eee1bbc71d2dcd6d1f08b7f3d22e597a75773
Mrunal Pardeshi <mrunalrajput16@gmail.com>
2025-05-20 19:53:20 +0530

    In Progress

--- Project 6 - Paetron Clone Using Next JS/get-me-a-chai/components/Navbar.js
+++ Project 6 - Paetron Clone Using Next JS/get-me-a-chai/components/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => {
       Signed in as {session.user.email} <br/>
       <button onClick={() => signOut()}>Sign out</button>
     </>
+   }
   return (
     <nav className='bg-gray-950 text-white flex justify-between px-4 h-15 items-center'>
             <div className="logo font-bold text-lg flex items-center justify-center gap-1">
@@ -26,7 +27,7 @@ const Navbar = () => {
 
         <div>
           <Link href={"/login"}>
-          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button></Link>
+          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2' onClick={signIn("github")}>Login</button></Link>
         </div>
     </nav> 
   )
